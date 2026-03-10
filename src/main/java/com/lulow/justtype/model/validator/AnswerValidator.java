8f736036975be8fd22b064da50359259d058eebe
Lulow08 <yostinomgdan08@gmail.com>
2026-03-09 20:09:21 -0500

    feature: add AnswerValidator class

---
+++ src/main/java/com/lulow/justtype/model/validator/AnswerValidator.java
@@ -0,0 +1,8 @@
+package com.lulow.justtype.model.validator;
+
+public class AnswerValidator extends ValidatorAdapter {
+    @Override
+    public void validateAnswer(String answer) {
+        System.out.println(answer);
+    }
+}
