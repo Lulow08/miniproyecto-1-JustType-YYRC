fca1139583b1020c0f0cb1484c9a0c872f0b7f40
Lulow08 <yostinomgdan08@gmail.com>
2026-03-09 22:15:48 -0500

    feature: add TimerAdapter class

---
+++ src/main/java/com/lulow/justtype/model/timer/TimerAdapter.java
@@ -0,0 +1,7 @@
+package com.lulow.justtype.model.timer;
+
+public abstract class TimerAdapter implements ITimer {
+    @Override public void start(int seconds) {};
+    @Override public void stop() {};
+    @Override public int getSecondsLeft() { return 0; };
+}
