3f3a3d663315b7997bc2a95995734c1411c811d7
Lulow08 <yostinomgdan08@gmail.com>
2026-03-09 21:31:12 -0500

    feature: use GameLogic and GameView new functionalities

--- src/main/java/com/lulow/justtype/controller/GameController.java
+++ src/main/java/com/lulow/justtype/controller/GameController.java
@@ -1,18 +1,32 @@
 package com.lulow.justtype.controller;
 
 import com.lulow.justtype.model.GameLogic;
+import com.lulow.justtype.view.GameView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
-import java.io.IOException;
+import javafx.scene.layout.HBox;
 
 public class GameController {
 
     @FXML private TextField inputField;
-
-    // TODO: add initialize
+    @FXML private HBox      wordDisplay;
+    @FXML private Label     levelLabel;
+    @FXML private Label     timerLabel;
 
     private GameLogic gameLogic = new GameLogic();
+    private GameView gameView;
+
+    @FXML
+    public void initialize() {
+        gameView = new GameView(wordDisplay, levelLabel, timerLabel);
+        gameLogic.nextWord();
+        refreshUI();
+
+        inputField.textProperty().addListener((obs, oldText, newText) ->
+                gameView.colorizeChars(newText, gameLogic.getCurrentWord())
+        );
+    }
 
     @FXML
     private void onHandleSubmitButton() { submitAnswer(inputField.getText()); }
@@ -21,7 +35,18 @@ public class GameController {
     private void onHandleEnter() { submitAnswer(inputField.getText()); }
 
     private void submitAnswer(String input) {
-        gameLogic.processAnswer(input);
+        boolean correct = gameLogic.processAnswer(input);
+
+        if (correct) { gameLogic.levelUp(); }
+        else { gameLogic.reset(); }
+
+        gameLogic.nextWord();
         inputField.clear();
+        refreshUI();
+    }
+
+    private void refreshUI() {
+        gameView.renderWord(gameLogic.getCurrentChars());
+        gameView.updateHUD(gameLogic.getCurrentLevel(), gameLogic.getTimeForCurrentLevel());
     }
 }
