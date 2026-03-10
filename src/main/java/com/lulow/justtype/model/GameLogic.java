5bd44850d424765c82532d740af2a6f5553b88a3
Lulow08 <yostinomgdan08@gmail.com>
2026-03-09 21:30:30 -0500

    feature: add basic game logic

--- src/main/java/com/lulow/justtype/model/GameLogic.java
+++ src/main/java/com/lulow/justtype/model/GameLogic.java
@@ -4,11 +4,43 @@ import com.lulow.justtype.model.validator.AnswerValidator;
 
 public class GameLogic {
 
-    private AnswerValidator answerValidator = new AnswerValidator();
+    public static final int INITIAL_TIME = 20;
+    public static final int MIN_TIME = 2;
+    private static final int LEVELS_PER_REDUCTION = 5;
+    private static final int TIME_REDUCTION = 2;
 
-    public void processAnswer(String input) {
-        answerValidator.validateAnswer(input); // TODO: turn into boolean type
+    private final WordBank wordBank = new WordBank();
+    private final WordSplitter wordSplitter = new WordSplitter();
+    private final AnswerValidator answerValidator = new AnswerValidator();
 
-        // Handle logic depending on validation
+    private int currentLevel = 1;
+    private String currentWord = "";
+    private char[] currentChars;
+
+    public char[] getCurrentChars() { return currentChars; }
+    public String getCurrentWord() { return currentWord; }
+    public int getCurrentLevel() { return currentLevel; }
+
+    public void nextWord() {
+        currentWord = wordBank.getRandomWord(currentLevel);
+        currentChars = wordSplitter.split(currentWord);
+    }
+
+    public boolean processAnswer(String input) {
+        return answerValidator.validateAnswer(input, currentWord);
+    }
+
+    public void levelUp() {
+        currentLevel++;
+    }
+
+    public void reset() {
+        currentLevel = 1;
+    }
+
+    public int getTimeForCurrentLevel() {
+        int reductions = (currentLevel - 1) / LEVELS_PER_REDUCTION;
+        int time = INITIAL_TIME - (reductions * TIME_REDUCTION);
+        return Math.max(time, MIN_TIME);
     }
 }
