05d684f8d3cb8b21a23d373f3a619ff3e654fa70
Lulow08 <yostinomgdan08@gmail.com>
2026-03-11 20:47:23 -0500

    style: remove spaces

--- src/main/java/com/lulow/justtype/view/animations/PressAnimation.java
+++ src/main/java/com/lulow/justtype/view/animations/PressAnimation.java
@@ -7,7 +7,7 @@ import javafx.util.Duration;
 public class PressAnimation extends AnimatorAdapter {
 
     private static final int PRESS_DURATION_MS = 80;
-    private static final int RELEASE_DURATION_MS    = 100;
+    private static final int RELEASE_DURATION_MS = 100;
 
     private final ScaleTransition pressIn;
     private final ScaleTransition pressOut;
