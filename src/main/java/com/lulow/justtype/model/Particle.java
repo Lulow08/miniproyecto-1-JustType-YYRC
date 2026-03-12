edf37957ecb204c88ed462498c42556545ac76bd
Lulow08 <yostinomgdan08@gmail.com>
2026-03-11 20:06:13 -0500

    style: naming improvements

--- src/main/java/com/lulow/justtype/model/Particle.java
+++ src/main/java/com/lulow/justtype/model/Particle.java
@@ -4,12 +4,12 @@ import javafx.scene.shape.Rectangle;
 
 public class Particle {
 
-    private static final double GRAVITY = 0.16;
-    private static final double ROTATION_SPEED = 4.0;
-    private static final double MIN_X_VELOCITY = 1.0;
-    private static final double MIN_Y_VELOCITY = -2.0;
-    private static final double MAX_X_VELOCITY_VARIANCE = 3.0;
-    private static final double MAX_Y_VELOCITY_VARIANCE = -4.0;
+    private static final double GRAVITY             = 0.16;
+    private static final double ROTATION_SPEED      = 4.0;
+    private static final double BASE_X_VELOCITY     = 1.0;
+    private static final double BASE_Y_VELOCITY     = 2.0;
+    private static final double X_VELOCITY_VARIANCE = 3.0;
+    private static final double Y_VELOCITY_VARIANCE = 4.0;
 
     private double xPos, yPos, xVelocity, yVelocity;
     private final Rectangle particleRender;
@@ -19,14 +19,14 @@ public class Particle {
         this.yPos = yPos;
         this.particleRender = particleRender;
 
-        this.xVelocity = -direction * (MIN_X_VELOCITY + Math.random() * MAX_X_VELOCITY_VARIANCE);
-        this.yVelocity = MIN_Y_VELOCITY + Math.random() * MAX_Y_VELOCITY_VARIANCE;
+        this.xVelocity = -direction * (BASE_X_VELOCITY + Math.random() * X_VELOCITY_VARIANCE);
+        this.yVelocity = -(BASE_Y_VELOCITY + Math.random() * Y_VELOCITY_VARIANCE);
     }
 
     public void update() {
         yVelocity += GRAVITY;
-        xPos += xVelocity;
-        yPos += yVelocity;
+        xPos      += xVelocity;
+        yPos      += yVelocity;
 
         particleRender.setX(xPos);
         particleRender.setY(yPos);
