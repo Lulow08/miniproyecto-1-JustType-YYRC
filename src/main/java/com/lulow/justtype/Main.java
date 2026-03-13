61a3571040774edc915031693cf86af91d993db9
Lulow08 <yostinomgdan08@gmail.com>
2026-03-13 09:16:18 -0500

    feature: add favicon

--- src/main/java/com/lulow/justtype/Main.java
+++ src/main/java/com/lulow/justtype/Main.java
@@ -2,19 +2,24 @@ package com.lulow.justtype;
 
 import com.lulow.justtype.view.SceneManager;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
     public static void main(String[] args) { launch(args); }
 
     @Override
-    public void start(Stage mainStage) throws IOException {
+     public void start(Stage mainStage) throws IOException {
+
+        Image favicon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/favicon.png")));
 
         mainStage.setTitle("JustType");
         mainStage.setResizable(false);
+        mainStage.getIcons().add(favicon);
 
         SceneManager sceneManager = SceneManager.getInstance();
         sceneManager.setMainStage(mainStage);
