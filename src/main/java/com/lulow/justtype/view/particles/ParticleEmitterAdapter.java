9ebbe88cc3d6ae19f9950e3b914fd43e34f6a0e0
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:46:29 -0500

    feature: implement new methods from IParticleEmmiter

--- src/main/java/com/lulow/justtype/view/particles/ParticleEmitterAdapter.java
+++ src/main/java/com/lulow/justtype/view/particles/ParticleEmitterAdapter.java
@@ -2,4 +2,6 @@ package com.lulow.justtype.view.particles;
 
 public abstract class ParticleEmitterAdapter implements IParticleEmitter {
     @Override public void play(double seconds) {}
+    @Override public void stop() {}
+    @Override public void setIntensity(int particlesPerFrame, double speedMultiplier) {}
 }
