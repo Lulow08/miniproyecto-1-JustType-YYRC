7d1a29d798f08de2eb58edbd0fc639e9fe16ba58
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:40:35 -0500

    feature: add IParticle interface

---
+++ src/main/java/com/lulow/justtype/model/particles/IParticle.java
@@ -0,0 +1,11 @@
+package com.lulow.justtype.model.particles;
+
+import javafx.scene.shape.Rectangle;
+
+public interface IParticle {
+    void update();
+    boolean isDead();
+    double getXPos();
+    double getYPos();
+    Rectangle getRender();
+}
