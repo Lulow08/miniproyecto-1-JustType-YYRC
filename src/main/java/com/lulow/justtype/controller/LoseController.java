b845f46e96557aba1d3ff0b7cfd863cb3be83026
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:42:26 -0500

    feature: implements onGoMenu

--- src/main/java/com/lulow/justtype/controller/LoseController.java
+++ src/main/java/com/lulow/justtype/controller/LoseController.java
@@ -1,15 +1,12 @@
 package com.lulow.justtype.controller;
 
+import com.lulow.justtype.view.LoseView;
 import com.lulow.justtype.view.SceneManager;
-import com.lulow.justtype.view.animations.FadeInAnimation;
-import com.lulow.justtype.view.animations.OvershootAnimation;
-import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -20,40 +17,24 @@ public class LoseController {
     @FXML private Label      answerLabel;
     @FXML private Label      hintLabel;
 
+    private LoseView loseView;
+
     @FXML
     public void initialize() {
-        SceneManager sceneManager = SceneManager.getInstance();
-
-        HBox wordDisplay = sceneManager.getLoseWordDisplay();
-        if (wordDisplay != null) {
-            rootPane.getChildren().add(wordDisplay);
-            AnchorPane.setLeftAnchor(wordDisplay, 0.0);
-            AnchorPane.setRightAnchor(wordDisplay, 0.0);
-        }
-
-        answerLabel.setText("Your answer: " + sceneManager.getLoseAnswer());
-
-        titleLabel.setOpacity(0);
-        answerLabel.setOpacity(0);
-        hintLabel.setOpacity(0);
-
-        PauseTransition delay = new PauseTransition(Duration.seconds(1.6));
-        delay.setOnFinished(event -> {
-            titleLabel.setOpacity(1.0);
-            new OvershootAnimation(titleLabel, 3.0, 0.88).play();
-            new FadeInAnimation(answerLabel).play();
-            new FadeInAnimation(hintLabel).play();
-        });
-        delay.play();
+        loseView = new LoseView(rootPane, titleLabel, answerLabel, hintLabel);
 
         rootPane.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.ENTER)  onContinue();
             if (event.getCode() == KeyCode.ESCAPE) onGoMenu();
         });
 
-        rootPane.sceneProperty().addListener((observableValue, oldScene, newScene) -> {
+        rootPane.sceneProperty().addListener((obs, oldScene, newScene) -> {
             if (newScene != null) rootPane.requestFocus();
         });
+    }
+
+    public void setup(String word, String wrongAnswer, HBox wordDisplay) {
+        loseView.setup(wrongAnswer, wordDisplay);
     }
 
     private void onContinue() {
@@ -64,5 +45,11 @@ public class LoseController {
         }
     }
 
-    private void onGoMenu() {}
+    private void onGoMenu() {
+        try {
+            SceneManager.getInstance().loadScene("menu-view.fxml");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
