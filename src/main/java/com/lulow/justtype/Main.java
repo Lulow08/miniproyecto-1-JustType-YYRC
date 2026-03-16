2aa04837e5b500eeee4abb329b1677ad7b975a2d
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:44:24 -0500

    refactor: move logic to AppInitializer

--- src/main/java/com/lulow/justtype/Main.java
+++ src/main/java/com/lulow/justtype/Main.java
@@ -1,30 +1,17 @@
 package com.lulow.justtype;
 
-import com.lulow.justtype.view.SceneManager;
+import com.lulow.justtype.model.AppInitializer;
 import javafx.application.Application;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Main extends Application {
 
     public static void main(String[] args) { launch(args); }
 
     @Override
-     public void start(Stage mainStage) throws IOException {
-
-        Image favicon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/favicon.png")));
-
-        mainStage.setTitle("JustType");
-        mainStage.setResizable(false);
-        mainStage.getIcons().add(favicon);
-
-        SceneManager sceneManager = SceneManager.getInstance();
-        sceneManager.setMainStage(mainStage);
-
-        sceneManager.loadFonts();
-        sceneManager.loadScene("game-view.fxml");
+    public void start(Stage mainStage) throws IOException {
+        new AppInitializer().start(mainStage);
     }
 }
