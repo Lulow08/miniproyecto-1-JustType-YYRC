de5a18e6faef8a69503e2e37f2f05feb7b91dba6
Lulow08 <yostinomgdan08@gmail.com>
2026-03-16 14:36:39 -0500

    feature: add AudioClips class

---
+++ src/main/java/com/lulow/justtype/model/AudioClips.java
@@ -0,0 +1,16 @@
+package com.lulow.justtype.model;
+
+public final class AudioClips {
+
+    public static final String MENU_MUSIC      = "menu-music.mp3";
+    public static final String GAME_MUSIC     = "game-music.mp3";
+    public static final String WIN_MUSIC      = "win-music.mp3";
+    public static final String CRUNCH         = "crunch.mp3";
+    public static final String KEY_PRESS      = "key-press.mp3";
+    public static final String CRITICAL_BEEP  = "critical-beep.mp3";
+    public static final String LOSE_BOOM      = "lose-boom.mp3";
+    public static final String LOSE_WHIP      = "lose-whip.mp3";
+    public static final String CONFETTI_POP   = "confetti-pop.mp3";
+
+    private AudioClips() {}
+}
